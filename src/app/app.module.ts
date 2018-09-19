9526739ae7043e534822abbb76f6fc7169180296
Zhen <zbian002@ucr.edu>
2018-09-19 13:37:54 -0700

    Add forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
-import { 
-  MatInputModule, 
-  MatCardModule, 
+import {
+  MatInputModule,
+  MatCardModule,
   MatButtonModule,
   MatToolbarModule,
   MatExpansionModule
@@ -35,6 +35,6 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
+export class AppModule {
 
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 import { sharedStylesheetJitUrl } from "@angular/compiler";
-import { Post } from '../post.model';
+import {  NgForm } from "@angular/forms";
+import { PostsService } from "../posts.service";
 
 @Component({
     selector: 'app-post-create',
@@ -10,13 +11,13 @@ import { Post } from '../post.model';
 export class PostCreateComponent {
     enteredTitle = '';
     enteredContent = '';
-    @Output() postCreated = new EventEmitter<Post>();
 
-    onAddPost() {
-        const post: Post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
-        };
-        this.postCreated.emit(post);
+    constructor(public postsService: PostsService) {}
+
+    onAddPost(form: NgForm) {
+      if(form.invalid) {
+        return;
+      }
+      this.postsService.addPost(form.value.title, form.value.content);
     }
 }
