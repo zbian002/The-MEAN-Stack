1122cc08879946904a4c1fd4b18138ebdb4e8825
Zhen <zbian002@ucr.edu>
2018-09-18 19:25:13 -0700

    Add a toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component'
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from "@angular/material";
+import { 
+  MatInputModule, 
+  MatCardModule, 
+  MatButtonModule,
+  MatToolbarModule 
+} from "@angular/material";
+import { importExpr } from '@angular/compiler/src/output/output_ast';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -17,9 +25,12 @@ import { MatInputModule, MatCardModule, MatButtonModule } from "@angular/materia
     FormsModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+}
