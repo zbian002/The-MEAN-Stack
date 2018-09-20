aaa2ac4573e7900027eb08987dcd968409bb005a
Zhen <zbian002@ucr.edu>
2018-09-19 20:50:20 -0700

    Update app.js file

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: 'fadf12421l',
+      title: "First server-side post",
+      content: "This is coming from the server" },
+    { id: 'ksajflaj132',
+      title: "Second server-side post",
+      content: "This is coming from the server" }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
